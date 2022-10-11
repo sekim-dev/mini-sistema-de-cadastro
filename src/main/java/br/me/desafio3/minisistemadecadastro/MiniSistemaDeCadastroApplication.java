6cd43a6ba11ce871e99801ed536cb0f2be3ba819
Salomao kim <salomao.kim@me.com.br>
2022-10-11 12:04:42 -0300

    primeiro commit

---
+++ src/main/java/br/me/desafio3/minisistemadecadastro/MiniSistemaDeCadastroApplication.java
@@ -0,0 +1,13 @@
+package br.me.desafio3.minisistemadecadastro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniSistemaDeCadastroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniSistemaDeCadastroApplication.class, args);
+	}
+
+}
