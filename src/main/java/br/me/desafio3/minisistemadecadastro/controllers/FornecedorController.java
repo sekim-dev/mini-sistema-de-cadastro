b88858ed640949f88e1e88f5675d5144c0df368c
Salomao kim <salomao.kim@me.com.br>
2022-10-24 12:27:51 -0300

    teste heroku 1

--- src/main/java/br/me/desafio3/minisistemadecadastro/controllers/FornecedorController.java
+++ src/main/java/br/me/desafio3/minisistemadecadastro/controllers/FornecedorController.java
@@ -15,7 +15,6 @@ import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
-import java.util.Optional;
 
 @Controller
 //@RequestMapping(value = "/")
--- src/main/java/br/me/desafio3/minisistemadecadastro/controllers/IndexController.java
+++ src/main/java/br/me/desafio3/minisistemadecadastro/controllers/IndexController.java
@@ -3,7 +3,6 @@ package br.me.desafio3.minisistemadecadastro.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
@@ -11,7 +10,7 @@ public class IndexController {
     @GetMapping("/")
     public String index(Model model){
         model.addAttribute("msnBemVindo", "Bem-vindo ao mini sistema de cadastro");
-        return "/cadastro-index";
+        return "index";
     }
 
     @GetMapping("/login")
